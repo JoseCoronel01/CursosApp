b62c04e251d5685835da2b2529f08430d9490f6a
JoseCoronel01 <pepe2688@outlook.com>
2026-06-28 08:25:17 -0700

    fix update register DBGenerica program cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped<DBGenerica>();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
